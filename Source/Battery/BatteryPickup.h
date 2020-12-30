4e3fc1433c50f69bf8991dcf25578610c162bc84
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2020-12-30 16:15:23 -0300

    BatteryPickup Blueprint

---
+++ Source/Battery/BatteryPickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "BatteryPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTERY_API ABatteryPickup : public APickup
+{
+	GENERATED_BODY()
+	
+};
